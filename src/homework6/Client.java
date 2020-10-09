cd172ba156f87c3e0c60e7e2878c44a6c9c3b16e
samsuccess <samsuccess36@gmail.com>
2020-10-09 23:02:45 +0300

    change homework6

--- src/homework6/Client.java
+++ src/homework6/Client.java
@@ -1,4 +1,64 @@
 package homework6;
 
+import javafx.fxml.Initializable;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        final String IP_ADDRESS = "localhost";
+        final int PORT = 8189;
+
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+
+
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        System.out.println("Server: " + str);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            try {
+                while (true){
+//                    System.out.println("Client: ");
+                    out.writeUTF(scanner.nextLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            socket.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
--- src/homework6/Server.java
+++ src/homework6/Server.java
@@ -1,4 +1,61 @@
 package homework6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+
+
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        Socket socket = null;
+        final int PORT = 8189;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+            socket = server.accept();
+            System.out.println("Client connected");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        System.out.println("Client: " + str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+
+            try {
+                while (true) {
+//                    System.out.println("Server: ");
+                    out.writeUTF(scanner.nextLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            socket.close();
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
